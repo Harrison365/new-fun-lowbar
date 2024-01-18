f14041428e6a50f6a286e4ed2e95fbddd31910d1
Harrison Royds <harrisonroyds@yahoo.co.uk>
2024-01-18 11:04:35 +0000

    sumBy, maxBy and takeWhile added with tests and map tests added

--- __tests__/lowbar.test.js
+++ __tests__/lowbar.test.js
@@ -1,4 +1,12 @@
-const { identity, fromPairs, times } = require("../lowbar");
+const {
+  identity,
+  fromPairs,
+  sumBy,
+  maxBy,
+  times,
+  takeWhile,
+  map,
+} = require("../lowbar");
 
 // don't forget to export and import each of your new functions!
 
@@ -35,6 +43,79 @@ describe("fromPairs", () => {
   });
 });
 
+//////////sumBy///////////////////////////////////////////////////
+
+describe("sumBy", () => {
+  test("when given an empty array, returns 0", () => {
+    expect(
+      sumBy([], function (object) {
+        return object.n;
+      })
+    ).toBe(0);
+  });
+  test("when given an array of objects and a function, returns the sum of the values of the objects at specified key", () => {
+    expect(
+      sumBy([{ n: 4 }, { n: 2 }, { n: 8 }], function (object) {
+        return object.n;
+      })
+    ).toBe(14);
+  });
+  test("checks that the function is invoked the correct number of times with the correct arguments and return values", () => {
+    const mock = jest.fn((object) => object.n);
+    sumBy([{ n: 4 }, { n: 2 }, { n: 8 }], mock);
+    expect(mock).toHaveBeenCalledTimes(3);
+    expect(mock).toHaveBeenCalledWith({ n: 4 });
+    expect(mock).toHaveBeenCalledWith({ n: 2 });
+    expect(mock).toHaveBeenCalledWith({ n: 8 });
+    expect(mock).toHaveReturnedWith(4);
+    expect(mock).toHaveReturnedWith(2);
+    expect(mock).toHaveReturnedWith(8);
+  });
+  test("SHORTHAND TEST - when given an array of objects and a function, returns the sum of the values of the objects at specified key", () => {
+    expect(sumBy([{ n: 4 }, { n: 2 }, { n: 8 }], "n")).toBe(14);
+  });
+});
+
+//////////////maxBy///////////////////////////////////////////////////
+
+describe("maxBy", () => {
+  test("when given an empty array, returns 0", () => {
+    expect(
+      maxBy([], function (object) {
+        return object.n;
+      })
+    ).toBe(undefined);
+  });
+  test("when given an array with a single object, returns the single object", () => {
+    expect(
+      maxBy([{ n: 4 }], function (object) {
+        return object.n;
+      })
+    ).toEqual({ n: 4 });
+  });
+  test("when given an array of objects and a function, returns the max value of the objects at specified key", () => {
+    expect(
+      maxBy([{ n: 4 }, { n: 2 }, { n: 8 }], function (object) {
+        return object.n;
+      })
+    ).toEqual({ n: 8 });
+  });
+  test("SHORTHAND TEST - when given an array of objects and a function, returns the max value of the objects at specified key", () => {
+    expect(maxBy([{ n: 4 }, { n: 2 }, { n: 8 }], "n")).toEqual({ n: 8 });
+  });
+  test("checks that the function is invoked the correct number of times with the correct arguments and return values", () => {
+    const mock = jest.fn((object) => object.n);
+    maxBy([{ n: 4 }, { n: 2 }, { n: 8 }], mock);
+    expect(mock).toHaveBeenCalledTimes(3);
+    expect(mock).toHaveBeenCalledWith({ n: 4 });
+    expect(mock).toHaveBeenCalledWith({ n: 2 });
+    expect(mock).toHaveBeenCalledWith({ n: 8 });
+    expect(mock).toHaveReturnedWith(4);
+    expect(mock).toHaveReturnedWith(2);
+    expect(mock).toHaveReturnedWith(8);
+  });
+});
+
 //////////////times///////////////////////////////////////////////////
 
 describe("times", () => {
@@ -68,14 +149,117 @@ describe("times", () => {
     expect(times(2, mock)).toEqual([0, 1]);
     expect(mock).toHaveBeenCalledTimes(2);
     expect(mock).toHaveBeenCalledWith(1); //0 or 1
+    //toHaveReturnedWith is also a matcher, it checks the return value of the mock function
   });
 });
 
+//////////////takeWhile///////////////////////////////////////////////////
+
+describe("takeWhile", () => {
+  test("if array is empty, returns empty array", () => {
+    let funky = (thing) => {
+      return thing;
+    };
+    expect(takeWhile([], funky)).toEqual([]);
+  });
+  test("SHORTHAND TEST - takes array of objects and returns array of objects until it finds an object which doesnt match the object passed as the second arguement", () => {
+    expect(
+      takeWhile([{ n: true }, { n: true }, { n: false }, { n: true }], {
+        n: true,
+      })
+    ).toEqual([{ n: true }, { n: true }]);
+  });
+  test("SHORTHAND TEST - takes array of objects and returns array of objects until it finds an object which doesnt match the array passed as the second arguement", () => {
+    expect(
+      takeWhile(
+        [
+          { n: true, m: false },
+          { n: true, m: true },
+          { n: false, m: true },
+          { n: true, m: true },
+        ],
+        ["n", true]
+      )
+    ).toEqual([
+      { n: true, m: false },
+      { n: true, m: true },
+    ]);
+  });
+  test("SHORTHAND TEST - takes array of objects and returns array of objects until it finds an object which is falsey at the key passed as the second arguement", () => {
+    expect(
+      takeWhile(
+        [
+          { n: true, m: false },
+          { n: true, m: true },
+          { n: false, m: true },
+          { n: true, m: true },
+        ],
+        "n"
+      )
+    ).toEqual([
+      { n: true, m: false },
+      { n: true, m: true },
+    ]);
+  });
+});
+
+//////////////map///////////////////////////////////////////////////
+
+describe("map", () => {
+  test("if array is empty, returns empty array", () => {
+    let funky = (thing) => {
+      return thing;
+    };
+    expect(map([], funky)).toEqual([]);
+  });
+  test("takes an array and returns an array where each item has been passed through the function passed as the second arguement AND function called right amount of times with right arguments", () => {
+    let funky = jest.fn((thing) => {
+      return thing + 1;
+    });
+    expect(map([1, 2, 3], funky)).toEqual([2, 3, 4]);
+    expect(funky).toHaveBeenCalledTimes(3);
+    expect(funky).toHaveBeenCalledWith(1);
+    expect(funky).toHaveBeenCalledWith(2);
+    expect(funky).toHaveBeenCalledWith(3);
+    expect(funky).toHaveReturnedWith(2);
+    expect(funky).toHaveReturnedWith(3);
+    expect(funky).toHaveReturnedWith(4);
+  });
+  test("SHORTHAND TEST - takes an object and returns an array where object value has been passed through the function passed as the second arguement AND function called right amount of times with right arguments", () => {
+    let funky = jest.fn((thing) => {
+      return thing + 1;
+    });
+    expect(map({ a: 1, b: 2, c: 3 }, funky)).toEqual([2, 3, 4]);
+    expect(funky).toHaveBeenCalledTimes(3);
+    expect(funky).toHaveBeenCalledWith(1);
+    expect(funky).toHaveBeenCalledWith(2);
+    expect(funky).toHaveBeenCalledWith(3);
+    expect(funky).toHaveReturnedWith(2);
+    expect(funky).toHaveReturnedWith(3);
+    expect(funky).toHaveReturnedWith(4);
+  });
+
+  test("SHORTHAND TEST - takes an array of objects and returns an array of values from each object at the key specified in the second arguement", () => {
+    expect(
+      map(
+        [
+          { n: 4, m: 2 },
+          { n: 2, m: 2 },
+          { n: 8, m: 2 },
+        ],
+        "n"
+      )
+    ).toEqual([4, 2, 8]);
+  });
+});
+
+//CONSOLE.LOG TESTING
+
 //to spy on console.log calls vvv
-consoleLogSpy = jest.spyOn(console, "log");
-expect(consoleLogSpy.mock.calls[0][0]).toBe(
-  "What ever you expect the first log to be"
-);
-expect(consoleLogSpy.mock.calls[1][0]).toBe("...and the second log to be this");
-//to clear this spy vvv
-consoleLogSpy.mockRestore();
+// consoleLogSpy = jest.spyOn(console, "log");
+// expect(consoleLogSpy.mock.calls[0][0]).toBe(
+//   "What ever you expect the first log to be"
+// );
+// expect(consoleLogSpy.mock.calls[1][0]).toBe("...and the second log to be this");
+// //to clear this spy vvv
+// consoleLogSpy.mockRestore();
--- lowbar.js
+++ lowbar.js
@@ -9,12 +9,98 @@ function fromPairs(arrPairs) {
   return obj;
 }
 
+function sumBy(objects, func) {
+  let sum = 0;
+  for (let i = 0; i < objects.length; i++) {
+    if (typeof func === "function") {
+      sum += func(objects[i]);
+    } else if (typeof func === "string") {
+      sum += objects[i][func];
+    }
+  }
+  return sum;
+}
+
+function maxBy(objects, func) {
+  if (objects.length === 0) {
+    return undefined;
+  }
+  if (objects.length === 1) {
+    return objects[0];
+  }
+  let max = 0;
+  let maxObj = {};
+  for (let i = 0; i < objects.length; i++) {
+    if (typeof func === "function") {
+      if (func(objects[i]) > max) {
+        maxObj = objects[i];
+      }
+    } else if (typeof func === "string") {
+      if (objects[i][func] > max) {
+        maxObj = objects[i];
+      }
+    }
+  }
+  return maxObj;
+}
+
 function times(n, func) {
   let arr = [];
   for (let i = 0; i < n; i++) {
     arr.push(func(i));
   }
   return arr;
+}
+
+//if passed _.constant(0) as func...
+//_.constant returns a function which, no matter what its passed returns the outer functions arguement. Meaning ^^^this solution will be fine if the function arguement was for example _.constant(0) as it equates to a function which returns 0 what ever its passed
+
+function takeWhile(arr, func) {
+  //function to check that the keys and values of 2 objects match
+  function checkObjMatch(o1, o2) {
+    for (let key in o1) {
+      if (o1[key] !== o2[key]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  let newArr = [];
+  if (typeof func === "function") {
+    for (let i = 0; i < arr.length; i++) {
+      if (func(arr[i])) {
+        newArr.push(arr[i]);
+      } else {
+        return newArr;
+      }
+    }
+  } else if (typeof func === "object" && !Array.isArray(func)) {
+    for (let i = 0; i < arr.length; i++) {
+      if (checkObjMatch(arr[i], func)) {
+        newArr.push(arr[i]);
+      } else {
+        return newArr;
+      }
+    }
+  } else if (Array.isArray(func)) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i][func[0]] === func[1]) {
+        newArr.push(arr[i]);
+      } else {
+        return newArr;
+      }
+    }
+  } else if (typeof func === "string") {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i][func]) {
+        newArr.push(arr[i]);
+      } else {
+        return newArr;
+      }
+    }
+  }
+  return newArr;
 }
 
 function map(collection, action) {
@@ -44,16 +130,5 @@ function map(collection, action) {
   }
   return returnArr;
 }
-console.log(
-  map([1, 2], (a) => {
-    return a + 1;
-  })
-);
-console.log(
-  map({ a: 1, b: 2 }, (a) => {
-    return a + 1;
-  })
-);
-console.log(map([{ a: 2 }, { b: 2 }, { a: 3 }], "a"));
 
-module.exports = { identity, fromPairs, times };
+module.exports = { identity, fromPairs, sumBy, maxBy, times, takeWhile, map };
